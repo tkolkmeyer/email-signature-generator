8f53bc16be77853a8587d66b0a0299353dbbb88d
tkolkmeyer <tkolkmeyer@gmail.com>
2026-02-16 11:28:22 -0600

    app component complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { EmojiSweatComponent } from './emoji-sweat/emoji-sweat.component';
+import { EmojiNailsComponent } from './emoji-nails/emoji-nails.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, EmojiSweatComponent],
+  imports: [HomeComponent, EmojiSweatComponent, EmojiNailsComponent],
   template: `
     <main>
       <app-home></app-home>
       <app-emoji-sweat></app-emoji-sweat>
+      <app-emoji-nails></app-emoji-nails>
     </main>
   `,
 })
